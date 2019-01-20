72576969deb6031fa959f7344e65dde52a7a77bd
MaxB0u <45112368+MaxB0u@users.noreply.github.com>
2019-01-20 18:50:50 -0500

    Update BangBangController.java
    Final version before presentation on monday

--- Lab1/BangBangController.java
+++ Lab1/BangBangController.java
@@ -1,8 +1,14 @@
-package ca.mcgill.ecse211.HelloWorld;
+package ca.mcgill.ecse211.lab1;
 
 import lejos.hardware.motor.*;
 
-
+/**
+ * Class that implements the BangBang Controller model.
+ * If the robot is not in the right distance from the bandCenter (bandwidth)
+ * the robot will apply the maximum correction and turn to come back within the bandwidth
+ * @author maxbo
+ *
+ */
 
 public class BangBangController implements UltrasonicController {
 
@@ -17,7 +23,18 @@ public class BangBangController implements UltrasonicController {
   
 	private int distance;
 
+	private static final int FILTER_OUT = 15;
+	
+	private int filterControl;
  
+	/**
+	 * Class constructor. Creates a new instance of this controller.
+	 * Will be called in the WallFollowingLab class
+	 * @param bandCenter
+	 * @param bandwidth
+	 * @param motorLow
+	 * @param motorHigh
+	 */
 
 	public BangBangController(int bandCenter, int bandwidth, int motorLow, int motorHigh) {
     // Default Constructor
@@ -43,36 +60,79 @@ public class BangBangController implements UltrasonicController {
 	 }
 
   
-	
+	 /**
+	   * Defines the method of the interface to process
+	   * the data of the US sensor and determine the course of action
+	   * to take
+	   * @param distance
+	   * @return void
+	   */
 	@Override
-  
-
 	public void processUSData(int distance) {
     
 		this.distance = distance;
     // TODO: process a movement based on the us distance passed in (BANG-BANG style)
 		
-		if (Math.abs(distance - bandCenter) <= bandwidth) {
-			//go forward
-			WallFollowingLab.rightMotor.setSpeed(motorHigh);
-			WallFollowingLab.leftMotor.setSpeed(motorHigh);
-			
-			WallFollowingLab.leftMotor.forward();
-			WallFollowingLab.rightMotor.forward();
-			
-		} else if (distance - bandCenter >= bandwidth) {
-			//Too far from the wall
-			
+		 // rudimentary filter - toss out invalid samples corresponding to null
+	    // signal.
+	    // (n.b. this was not included in the Bang-bang controller, but easily
+	    // could have).
+	    //
+	    if (distance >= 255 && filterControl < FILTER_OUT) {
+	      // bad value, do not set the distance var, however do increment the
+	      // filter value
+	      filterControl++;
+	    } else if (distance >= 255) {
+	      // We have repeated large values, so there must actually be nothing
+	      // there: leave the distance alone
+	      this.distance = distance;
+	    } else {
+	      // distance went below 255: reset filter and leave
+	      // distance alone.
+	      filterControl = 0;
+	      this.distance = distance;
+	    }
+
+		
+		if (distance - bandCenter >= bandwidth) {
+			//too far from the wall, will turn left
 			WallFollowingLab.rightMotor.setSpeed(motorHigh);
 			WallFollowingLab.leftMotor.setSpeed(motorLow);
 			
 			WallFollowingLab.leftMotor.forward();
 			WallFollowingLab.rightMotor.forward();
-				
-		}else {
-			//too close from the wall
+			
+		} else if (distance < bandCenter - bandwidth && distance > bandCenter/2) {
+			//Too close from the wall, will turn right
 			
 			WallFollowingLab.rightMotor.setSpeed(motorLow);
+			WallFollowingLab.leftMotor.setSpeed(motorHigh);
+			
+			WallFollowingLab.leftMotor.forward();
+			WallFollowingLab.rightMotor.forward();
+				
+		}else if (distance < bandCenter/2) {
+			// Way too close from the wall, will turn right super fast
+			
+			WallFollowingLab.rightMotor.setSpeed(motorLow);
+			WallFollowingLab.leftMotor.setSpeed(motorLow);
+			
+			WallFollowingLab.leftMotor.forward();
+			WallFollowingLab.rightMotor.backward();
+		} 
+		else if (distance > 100){
+			//False negative, move awy from the wall
+			WallFollowingLab.rightMotor.setSpeed(motorLow);
+			WallFollowingLab.leftMotor.setSpeed(motorLow);
+			
+			WallFollowingLab.leftMotor.forward();
+			WallFollowingLab.rightMotor.backward();
+	    	
+	    }
+		else {
+			//go forward, the robot is within the bandwidth
+			
+			WallFollowingLab.rightMotor.setSpeed(motorHigh);
 			WallFollowingLab.leftMotor.setSpeed(motorHigh);
 			
 			WallFollowingLab.leftMotor.forward();
@@ -81,10 +141,15 @@ public class BangBangController implements UltrasonicController {
 			
 
   	}
+	
 
+	/**
+	   * Method used to calculate the speed correction depending on the distance from the bandcenter
+	   * Use a linear approach to calculate the error correction
+	   * @param errorValue
+	   * @return sppedCorrection
+	   */
   @Override
-  
-
 	public int readUSDistance() {
     
 		return this.distance;
@@ -92,3 +157,4 @@ public class BangBangController implements UltrasonicController {
 	}
 
 }
+
