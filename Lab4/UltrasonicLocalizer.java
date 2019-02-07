e3bb11461d63f51ef3f92e60877ef7bb1874f4db
Violet Wei <zhenzhou.wei@mail.mcgill.ca>
2019-02-07 15:27:09 -0500

    Create UltrasonicLocalizer.java

---
+++ Lab4/UltrasonicLocalizer.java
@@ -0,0 +1,9 @@
+/**
+ * class that performs the two ultrasonic localization routines: FallingEdge and RisingEdge
+ */
+ 
+ /* The method that performs the falling edge localization */
+ 
+ 
+ 
+ /* The method that performs the rising edge localization */
