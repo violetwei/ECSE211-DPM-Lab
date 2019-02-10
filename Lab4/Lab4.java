f0915514369b091b7844a8121986e436b18e3e3e
MaxB0u <45112368+MaxB0u@users.noreply.github.com>
2019-02-09 23:37:12 -0500

    Update Lab4.java

--- Lab4/Lab4.java
+++ Lab4/Lab4.java
@@ -2,7 +2,8 @@ package ca.mcgill.ecse211.lab4;
 
 
 
-  import lejos.hardware.Button;
+  
+import lejos.hardware.Button;
   import lejos.hardware.ev3.LocalEV3;
   import lejos.hardware.lcd.TextLCD;
   import lejos.hardware.motor.EV3LargeRegulatedMotor;
@@ -34,24 +35,19 @@ package ca.mcgill.ecse211.lab4;
     public static Odometer odometer = new Odometer();
     public static final double WHEEL_RAD = 2.1;
     public static final double TRACK = 11.2;
-    public static final int SPEED = 100;
-    public static final double SQUARE_LENGTH = 30.48;
+    public static final int SPEED = 75;
+    public static boolean risingEdge;
+
+    public enum LocalizationType { FALLING_EDGE, RISING_EDGE };
     
-    //Parameters to be accessed directly in the navigation class
-    public static int NUMPOINTS = 4;
-    public static boolean clockwise = false;
-    //The four different maps are entered here and only the one being currently use in not in comments
-   
-    //public static double[][] four_points = {{0.0,2.0},{1.0,1.0},{2.0,2.0},{2.0,1.0},{1.0,0.0}};
-    //public static double[][] four_points = {{1.0,1.0},{0.0,2.0},{2.0,2.0},{2.0,1.0},{1.0,0.0}};
-    //public static double[][] four_points = {{1.0,0.0},{2.0,1.0},{2.0,2.0},{0.0,2.0},{1.0,1.0}};     //last turn doesn't work, go look in turn conditions!!
-    //public static double[][] four_points = {{0.0,1.0},{1.0,2.0},{1.0,0.0},{2.0,1.0},{2.0,2.0}};
+    private static final Port usPort = LocalEV3.get().getPort("S1");
 
-    public static double[][] four_points = {{2.0,1.0},{1.0,1.0},{1.0,2.0},{2.0,0.0}};
-
-    private static final Port usPort = LocalEV3.get().getPort("S2");
-
-
+    @SuppressWarnings("resource") // Because we don't bother to close this resource
+    public static SensorModes usSensor = new EV3UltrasonicSensor(usPort); // usSensor is the instance
+    public static SampleProvider usDistance = usSensor.getMode("Distance"); // usDistance provides samples provider from
+    // this instance
+    public static float[] usData = new float[usDistance.sampleSize()]; // usData is the buffer in which data is
+    // returned in
 
     /**
      * Main entry of the program. Starts the run and lets the user select between
@@ -67,12 +63,7 @@ package ca.mcgill.ecse211.lab4;
       // implementation
       Display odometryDisplay = new Display(); // No need to change
 
-      @SuppressWarnings("resource") // Because we don't bother to close this resource
-      SensorModes usSensor = new EV3UltrasonicSensor(usPort); // usSensor is the instance
-      SampleProvider usDistance = usSensor.getMode("Distance"); // usDistance provides samples provider from
-      // this instance
-      float[] usData = new float[usDistance.sampleSize()]; // usData is the buffer in which data is
-      // returned in
+      
 
 
 
@@ -81,16 +72,16 @@ package ca.mcgill.ecse211.lab4;
         LCD.clear();
 
         // ask the user what program he wants to run
-        LCD.drawString("   Press enter       ", 0, 0);
-        LCD.drawString("    to start         ", 0, 1);
-        LCD.drawString("   the program       ", 0, 2);
-        LCD.drawString("                     ", 0, 3);
+        LCD.drawString("   Press left        ", 0, 0);
+        LCD.drawString(" for Falling edge    ", 0, 1);
+        LCD.drawString("   Press right       ", 0, 2);
+        LCD.drawString(" for Rising edge     ", 0, 3);
         LCD.drawString(" Left to test dist   ", 0, 4);
         LCD.drawString("    Press down       ", 0, 5);
         LCD.drawString(" to test rotation    ", 0, 6);
 
         buttonChoice = Button.waitForAnyPress(); //wait for the user
-      } while (buttonChoice != Button.ID_ENTER && buttonChoice != Button.ID_DOWN && buttonChoice != Button.ID_LEFT); //Exit when a valid option has been selected
+      } while (buttonChoice != Button.ID_ENTER && buttonChoice != Button.ID_DOWN && buttonChoice != Button.ID_LEFT && buttonChoice != Button.ID_RIGHT); //Exit when a valid option has been selected
 
 
       // Start odometer and display threads
@@ -100,10 +91,15 @@ package ca.mcgill.ecse211.lab4;
       odoDisplayThread.start();
 
       if (buttonChoice == Button.ID_ENTER) {
-        // clear the display
+        
         LCD.clear();
-        /*Navigation navigation = new Navigation(usDistance, usData);
-        navigation.start(); //starts the navigation thread*/
+        (new Thread() {
+          public void run() {
+            TestClass.forward_2_tiles();
+          }
+        }).start();
+        
+       
 
       } else if (buttonChoice == Button.ID_DOWN) {
         //testing the rotation
@@ -115,78 +111,33 @@ package ca.mcgill.ecse211.lab4;
         }).start(); //starts the thread
       } else if (buttonChoice == Button.ID_LEFT) {
 
+     // clear the display
         LCD.clear();
-        (new Thread() {
-          public void run() {
-            TestClass.forward_2_tiles();
-          }
-        }).start();
         
-      }
+        
+        UltrasonicLocalizer USL = new UltrasonicLocalizer(LocalizationType.FALLING_EDGE);
+        USL.start();
+       
+        
+      }else if (buttonChoice == Button.ID_RIGHT) {
+
+        // clear the display
+           LCD.clear();
+           
+           
+           UltrasonicLocalizer USL = new UltrasonicLocalizer(LocalizationType.RISING_EDGE);
+           USL.start();
+         
+           
+         }
 
 
       while (Button.waitForAnyPress() != Button.ID_ESCAPE); //to end program
       System.exit(0);
     }
 
-    /**
-     * Getter method for the left motor
-     * 
-     * @return leftMotor
-     */
-    public static EV3LargeRegulatedMotor getLeftMotor(){
-      return leftMotor;
-    }
-
-    /**
-     * Getter method for the right motor
-     * 
-     * @return rightMotor
-     */
-    public static EV3LargeRegulatedMotor getRightMotor(){
-      return rightMotor;
-    }
-
-    /**
-     * Getter method for the radius of the wheels
-     * 
-     * @return WHEEL_RAD
-     */
-    public static double getWR() {
-      return WHEEL_RAD;
-    }
-
-    /**
-     * Getter method for the wheel base of the robot
-     * 
-     * @return TRACK
-     */
-    public static double getTrack() {
-      return TRACK;
-    }
-
-    /**
-     * Getter method for the odometer instance
-     * 
-     * @return odometer
-     */
-    public static Odometer getOdo(){
-      return odometer;
-    }
-
-    /**
-     * Getter method for the LCD screen of the EV3
-     * 
-     * @return LCD
-     */
-    public static TextLCD getLCD(){
-      return LCD;
-    }
-
-   
-
+    
   }
-
 
 
 
