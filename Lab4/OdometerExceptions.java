b329bd6998e5ae192de1db84445a569dad8898be
MaxB0u <45112368+MaxB0u@users.noreply.github.com>
2019-02-12 00:43:35 -0500

    Update OdometerExceptions.java

--- Lab4/OdometerExceptions.java
+++ Lab4/OdometerExceptions.java
@@ -2,20 +2,22 @@ package ca.mcgill.ecse211.lab4;
 
 /**
  * This class is used to handle errors regarding the singleton pattern used for the odometer and
+ * odometerDatapackage ca.mcgill.ecse211.lab4;
+
+/**
+ * This class is used to handle errors regarding the singleton pattern used for the odometer and
  * odometerData
  *
  * @author Maxime Bourassa
  * @author Violet Wei
  */
-
 @SuppressWarnings("serial")
-
 public class OdometerExceptions extends Exception {
-  
- /**
-  * constructor of the class
-  * @param Error
-  */
+
+  /**
+   * constructor of the class
+   * @param Error
+   */
   public OdometerExceptions(String Error) {
     super(Error);
   }
