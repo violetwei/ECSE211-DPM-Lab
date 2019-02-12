f0eb609ebda4d59b5229e5d970e803da56e1fcee
MaxB0u <45112368+MaxB0u@users.noreply.github.com>
2019-02-12 00:41:13 -0500

    Update LightLocalizer.java

--- Lab4/LightLocalizer.java
+++ Lab4/LightLocalizer.java
@@ -12,6 +12,23 @@ import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.SensorModes;
 import lejos.robotics.SampleProvider;
 
+/**
+ * Class that implements the light localization
+ * The robot will localize more precisely after the ultrasonic
+ * localization using a light sensor and this algorithm
+ * Runs in a thread
+ * 
+ * {@value #oldValue} used in the differential filter to calculate the difference between the current and the last values returned by the light sensor
+ * {@value #correctionStart} time at the beginning of the loop 
+ * {@value #correctionEnd} time t the end of the loop
+ * {@value #DIFF_THRESHOLD} the differential threshold under which we assess that we have crossed a line
+ * {@value #DISTANCE_FROM_CENTER} The distance between the light sensor and the middle of the robot
+ * {@value #CORRECTION_PERIOD} The period of each loop
+ * {@value #LINE_HALF_ANGLE} half angle of a black line 
+ * 
+ * @author Maxime Bourassa
+ * @author Violet Wei
+ */
 public class LightLocalizer extends Thread {
 
   public static final Port colorSampler = LocalEV3.get().getPort("S2");
@@ -19,68 +36,51 @@ public class LightLocalizer extends Thread {
   public SampleProvider colorSensorValue = colosSamplerSensor.getMode("Red");
   public float[] colorSensorData = new float[colosSamplerSensor.sampleSize()];
   private float oldValue = 0;
-  private static final double DISTANCE_FROM_CENTER = 6.5;
   long correctionStart, correctionEnd;
   private int DIFF_THREASHOLD = -25;
-
+  private static final double DISTANCE_FROM_CENTER = 6.5;
   private static final long CORRECTION_PERIOD = 50;
+  private static final int LINE_HALF_ANGLE = 3;
+
+  /**
+   * Run method. Entry point of the thread
+   * Runs the correction until it calls other methods for the 
+   * smaller adjustments
+   * Uses a differential filter to spot lines
+   */
   public void run() {
-    boolean firsttime = true;
-
-
-
+    //set the speeds of the motors
     Nav.setSpeeds(SPEED);
+    //turns 45 degrees to face approximately the cornerof the tile
     Nav.turnTo(45);
-
-    leftMotor.forward();
-    rightMotor.forward();
-
-
-
-
-
+    //move forward
+    Nav.goForward();
+    //runs in a loop until a line is seen
     while (true) {
+      //keeps track of time
       correctionStart = System.currentTimeMillis();
-
       //fetching the values from the color sensor
       colorSensorValue.fetchSample(colorSensorData, 0);
-
       //getting the value returned from the sensor, and multiply it by 1000 to scale
       float value = colorSensorData[0]*1000;
-
       //computing the derivative at each point
       float diff = value - oldValue;
-
       //storing the current value, to be able to get the derivative on the next iteration
       oldValue = value;
-
-      //if the derivative value at a given point is less than -50, this means that a black line is detected
+      //if the derivative value at a given point is less than -25, this means that a line has been detected
       if(diff < DIFF_THREASHOLD) {
-
-        //robot beeps
+        //EV3 beeps
         Sound.beep();
-
-        leftMotor.stop(true);
-        rightMotor.stop(true);
-
-        if(firsttime) {
-          Nav.setSpeeds(SPEED);
-          leftMotor.rotate(Nav.convertDistance(DISTANCE_FROM_CENTER),true);
-          rightMotor.rotate(Nav.convertDistance(DISTANCE_FROM_CENTER),false);
-          firsttime = false;
-
-
-          precorrect();
-          break;
-        }
-
-
-
+        //stop the motors
+        Nav.stopMotors();
+        Nav.setSpeeds(SPEED);
+        leftMotor.rotate(Nav.convertDistance(DISTANCE_FROM_CENTER),true);
+        rightMotor.rotate(Nav.convertDistance(DISTANCE_FROM_CENTER),false);
+        //begins true correction
+        precorrect();
+        //exit the loop after method returns
+        break;
       }
-
-
-
-
       correctionEnd = System.currentTimeMillis();
       if (correctionEnd - correctionStart < CORRECTION_PERIOD) {
         try {
@@ -95,47 +95,43 @@ public class LightLocalizer extends Thread {
 
   }
 
+  /**
+   * This method is used after the robot detected its first line.
+   * At that point the error is only either in x or y but not both
+   * Here we determine if we need to correct x or y
+   * Uses a differential filter to spot lines
+   */
   private void precorrect() {
-
+    //Skip the 0 degree line
+    //and maybe the 270 degrees line depending on what coordinate needs correction
     Nav.turnTo(-120);
-    leftMotor.backward();
-    rightMotor.forward();
+    Nav.turnAnticlockwise();
+    //runs in a loop until a line is seen
     while(true) {
       correctionStart = System.currentTimeMillis();
       //fetching the values from the color sensor
       colorSensorValue.fetchSample(colorSensorData, 0);
-
       //getting the value returned from the sensor, and multiply it by 1000 to scale
       float value = colorSensorData[0]*1000;
-
       //computing the derivative at each point
       float diff = value - oldValue;
-
       //storing the current value, to be able to get the derivative on the next iteration
       oldValue = value;
-
-
-      //if the derivative value at a given point is less than -50, this means that a black line is detected
+      //if the derivative value at a given point is less than -25, this means that a line is detected
       if(diff < DIFF_THREASHOLD) {
-
-        //robot beeps
+        //beeps
         Sound.beep();
-
-        leftMotor.stop(true);
-        rightMotor.stop(true);
-
+        Nav.stopMotors();
+        //we detected the line on the left (270 degrees)
         if (odometer.getTheta() < 285 && odometer.getTheta() > 195) {
-
+          //x needs to be corrected
           correctX();
           break;
-
+          //we detected the 180 degree line
         } else if (odometer.getTheta() > 165) {
-
-
-
+          //y needs to be corrected
           correctY();
           break;
-
         }
       }
       correctionEnd = System.currentTimeMillis();
@@ -152,10 +148,12 @@ public class LightLocalizer extends Thread {
     }
   }
 
+  /**
+   * Method that implements the x correction
+   * Uses a differential filter to spot lines
+   */
   private void correctX() {
-
-    boolean firsttime = true;
-    boolean secondtime = false;
+    int numlines = 0;
     double distanceCorr = 0;
     leftMotor.forward();
     rightMotor.backward();
@@ -170,31 +168,23 @@ public class LightLocalizer extends Thread {
       correctionStart = System.currentTimeMillis();
       //fetching the values from the color sensor
       colorSensorValue.fetchSample(colorSensorData, 0);
-
       //getting the value returned from the sensor, and multiply it by 1000 to scale
       float value = colorSensorData[0]*1000;
-
       //computing the derivative at each point
       float diff = value - oldValue;
-
       //storing the current value, to be able to get the derivative on the next iteration
       oldValue = value;
-
-
-      //if the derivative value at a given point is less than -50, this means that a black line is detected
+      //if the derivative value at a given point is less than -25, this means that a black line is detected
       if(diff < DIFF_THREASHOLD) {
-
-        if(firsttime) {
-          //robot beeps
+        //first line spotted (0 degree line)
+        if(numlines == 0) {
+          //beeps
           Sound.beep();
-
+          //calculate the correction needed
           distanceCorr = Math.sin(odometer.getTheta()*Math.PI/180)*DISTANCE_FROM_CENTER;
-
-          leftMotor.forward();
-          rightMotor.backward();
-
-          firsttime = false;
-          secondtime = true;
+          Nav.turnClockwise();
+          numlines++;
+          //wait 1 second to avoid seeing the same line twice
           try {
             Thread.sleep(1000);
           } catch (InterruptedException e) {
@@ -202,30 +192,27 @@ public class LightLocalizer extends Thread {
             // expected that the odometry correction will be
             // interrupted by another thread
           }
-
-        } else if(secondtime) {
+          //90 degrees line spotted
+        } else if(numlines == 1) {
           Sound.beep();
-          leftMotor.stop(true);
-          rightMotor.stop(true);
-          leftMotor.forward();
-          rightMotor.forward();
+          //correct the error
+          Nav.stopMotors();
+          Nav.goForward();
           leftMotor.rotate(Nav.convertDistance(distanceCorr),true);
           rightMotor.rotate(Nav.convertDistance(distanceCorr),false);
-          leftMotor.backward();
-          rightMotor.forward();
-          secondtime = false;
-
-
-
+          Nav.turnAnticlockwise();
+          numlines++;
+          //90 degrees line spotted again
         } else {
-
-          Nav.turnTo(-90-3);
-          leftMotor.stop(true);
-          rightMotor.stop(true);
+          //as I always spot the beginning of lines, I need to add an offset of half of a black line's angle
+          //to my turn
+          Nav.turnTo(-90-LINE_HALF_ANGLE);
+          Nav.stopMotors();
+          //we are done, reset the odometer to 0
           odometer.setPosition(new double [] {0.0, 0.0, 0}, new boolean [] {true, true, true});
+          //exit the loop
           break;
         }
-
       }
       correctionEnd = System.currentTimeMillis();
       if (correctionEnd - correctionStart < CORRECTION_PERIOD) {
@@ -238,17 +225,22 @@ public class LightLocalizer extends Thread {
         }
       }
     }
-
   }
 
-
+  /**
+   * Method used to implement the Y correction of the robot
+   * Very similar to the X correction but some non trivial differences
+   * stopped us from combining the methods
+   * Implements a differential filter
+   */
   private void correctY() {
-    boolean firsttime = true;
-    boolean secondtime = false;
+    int numlines = 0;
     double distanceCorr = 0;
-    leftMotor.backward();
-    rightMotor.forward();
+    Nav.turnAnticlockwise();
+    //we are at the 180 degree line after the previous metohd
+    //correct the angle on the odometer
     odometer.setTheta(180);
+    //makes sure we don't spot the same line twice
     try {
       Thread.sleep(1000);
     } catch (InterruptedException e) {
@@ -261,42 +253,33 @@ public class LightLocalizer extends Thread {
       correctionStart = System.currentTimeMillis();
       //fetching the values from the color sensor
       colorSensorValue.fetchSample(colorSensorData, 0);
-
       //getting the value returned from the sensor, and multiply it by 1000 to scale
       float value = colorSensorData[0]*1000;
-
       //computing the derivative at each point
       float diff = value - oldValue;
-
       //storing the current value, to be able to get the derivative on the next iteration
       oldValue = value;
-
-
-      //if the derivative value at a given point is less than -50, this means that a black line is detected
+      //if the derivative value at a given point is less than -25, this means that a black line is detected
       if(diff < DIFF_THREASHOLD) {
-
-        if(firsttime) {
-          //robot beeps
+        //90 degrees line spotted
+        if(numlines == 0) {
+          //beeps
           Sound.beep();
-
+          //calculates the correction
           distanceCorr = Math.cos(odometer.getTheta()*Math.PI/180)*DISTANCE_FROM_CENTER;
-
-          firsttime = false;
-          secondtime = true;
+          numlines++;
           try {
             Thread.sleep(1000);
           } catch (Exception e) {
           } // Poor man's timed sampling
-
-        } else if(secondtime) {
-
+          // 0 degrees line spotted
+        } else if(numlines == 1) {
           Sound.beep();
-          leftMotor.stop(true);
-          rightMotor.stop(true);
-          leftMotor.forward();
-          rightMotor.forward();
+          Nav.stopMotors();
+          Nav.goForward();
           leftMotor.rotate(Nav.convertDistance(distanceCorr),true);
           rightMotor.rotate(Nav.convertDistance(distanceCorr),false);
+          //we are done, reset the odometer to 0
           odometer.setPosition(new double [] {0.0, 0.0, 0}, new boolean [] {true, true, true});
           break;
         }
